a5ec2264a110a71854f0971efe16285f06251214
Joe Spicuzza <spicuzza21@gmail.com>
2016-04-28 19:32:06 -0400

    final

---
+++ Assets/__Scripts/Shot.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable] // Make this visible in the Inspector
+public class Shot { // Shot does not extend MonoBehaviour
+
+	static public List<Shot>    shots = new List<Shot>(); // List of all shots
+	static public string        prefsName = "QuickSnap_Shots";
+	public Vector3        position; // Position of the Camera
+	public Quaternion     rotation; // Rotation of the Camera
+	public Vector3        target;   // Where the Camera is pointed
+
+	// Generates a single-line <shot> entry for an XML document
+	public string ToXML() {
+		string ss = "<shot ";
+		ss +=  "x=\""+position.x+"\" ";                                      // 1
+		ss +=  "y=\""+position.y+"\" ";
+		ss +=  "z=\""+position.z+"\" ";
+		ss += "qx=\""+rotation.x+"\" ";
+		ss += "qy=\""+rotation.y+"\" ";
+		ss += "qz=\""+rotation.z+"\" ";
+		ss += "qw=\""+rotation.w+"\" ";
+		ss += "tx=\""+target.x+"\" ";
+		ss += "ty=\""+target.y+"\" ";
+		ss += "tz=\""+target.z+"\" ";
+		ss += " />";
+		
+		return(ss);
+		}
+		// Takes a PT_XMLHashtable from PT_XMLReader of a <shot> entry in XML and
+		//  parses it into a Shot
+	static public Shot ParseShotXML( PT_XMLHashtable xHT ) {
+		Shot sh = new Shot();
+		
+		sh.position.x = float.Parse(xHT.att("x"));
+		sh.position.y = float.Parse(xHT.att("y"));
+		sh.position.z = float.Parse(xHT.att("z"));
+		sh.rotation.x = float.Parse(xHT.att("qx"));
+		sh.rotation.y = float.Parse(xHT.att("qy"));
+		sh.rotation.z = float.Parse(xHT.att("qz"));
+		sh.rotation.w = float.Parse(xHT.att("qw"));
+		sh.target.x   = float.Parse(xHT.att("tx"));
+		sh.target.y   = float.Parse(xHT.att("ty"));
+		sh.target.z   = float.Parse(xHT.att("tz"));
+		
+		return( sh );
+	}
+	
+	// Loads all of the Shots from PlayerPrefs
+	static public void LoadShots() {
+		// Empty the shots List<Shot>
+		shots = new List<Shot>();
+		
+		if (!PlayerPrefs.HasKey(prefsName)) {
+			// If there are no shots, return
+			return;
+		}
+		
+		// Get the full XML and parse it
+		string shotsXML = PlayerPrefs.GetString(prefsName);
+		PT_XMLReader xmlr = new PT_XMLReader();
+		xmlr.Parse(shotsXML);
+		
+		// Pull the PT_XMLHashList of all <shot>s
+		PT_XMLHashList hl = xmlr.xml["xml"][0]["shot"];
+		for (int i=0; i<hl.Count; i++) {
+			// Parse each <shot> in the PT_XMLHashlist
+			PT_XMLHashtable ht = hl[i];
+			Shot sh = ParseShotXML(ht);
+			// Add it to the List<shot> shots
+			shots.Add(sh);
+		}
+	}
+	
+	// Save List<Shot> shots to PlayerPrefs
+	static public void SaveShots() {
+		string xs = Shot.XML;
+		
+		Utils.tr(xs); // Trace all the XML to the Console
+		
+		// Set the PlayerPrefs
+		PlayerPrefs.SetString(prefsName, xs);
+		
+		Utils.tr("PlayerPrefs."+prefsName+" has been set.");
+	}
+	
+	// Convert all Shot.shots to XML
+	static public string XML {
+		get {
+			// Start an XML string
+			string xs = "<xml>\n";
+			// Add each of the Shots as a <shot> in XML
+			foreach( Shot sh in shots ) {
+				xs += sh.ToXML()+"\n";
+			}
+			// Add the closing XML tag
+			xs += "</xml>";
+			return(xs);
+		}
+	}
+	// Delete Shots from Shot.shots and PlayerPrefs
+	static public void DeleteShots() {
+		shots = new List<Shot> ();
+		if (PlayerPrefs.HasKey (prefsName)) {
+			PlayerPrefs.DeleteKey (prefsName);
+			Utils.tr ("PlayerPrefs." + prefsName + " has been deleted.");
+		} else {
+			Utils.tr ("There was no PlayerPrefs." + prefsName + " to delete."); 
+		}
+	}
+	// Replace the shot
+	static public void ReplaceShot(int ndx, Shot sh) {
+		// Make sure there's a Shot at that index to replace
+		if (shots==null || shots.Count <= ndx) return;
+		// Remove the old Shot
+		shots.RemoveAt(ndx);
+		// List<>.Insert() adds something to the list at a specific index
+		shots.Insert(ndx,sh);
+		
+		Utils.tr("Replaced shot:", ndx, "with", sh.ToXML());
+	}
+
+	// Compare two Shots. 1 is a perfect match, while <0 is not valid
+	public static float Compare (Shot target, Shot test) {
+		// Get the positinal deviation of both the camera and the Raycast hit
+		float posDev = (test.position - target.position).magnitude;
+		float tarDev = (test.target - target.target).magnitude;
+		
+		float posAccPct, tarAccPct, posAP2, tarAP2; // Accuracy percentages
+		TargetCamera tc = TargetCamera.S;
+		
+		// Get a value for accuracy where 1 is perfect and 0 is barely ok
+		posAccPct = 1-(posDev/tc.maxPosDeviation); 
+		tarAccPct = 1-(tarDev/tc.maxTarDeviation);
+		
+		// Curve the value so that it's more forgiving. This uses the same
+		//  Easing that we do for motion. You can curve ANY value between
+		//  0 and 1, not just Interpolation values.
+		posAP2 = Easing.Ease(posAccPct, tc.deviationEasing);
+		tarAP2 = Easing.Ease(tarAccPct, tc.deviationEasing);
+		
+		float accuracy = (posAP2+tarAP2)/2f;
+		
+		// Remember that you can use Utils to format numbers nicely as strings
+		string accText = Utils.RoundToPlaces(accuracy*100).ToString()+"%";
+		Utils.tr("Position:",posAccPct,posAP2,"Target:",tarAccPct,tarAP2,"Accuracy",accText);
+		
+		return(accuracy);
+	} 
+}
---
+++ Assets/__Scripts/TargetCamera.cs
@@ -0,0 +1,218 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TargetCamera : MonoBehaviour {
+	static public TargetCamera S;
+	public bool editMode = true;
+	public GameObject        fpCamera; // First-person Camera
+	// Maximum deviation in Shot.position allowed
+		public float            maxPosDeviation = 1f;
+	// Maximum deviation in Shot.target allowed
+	public float            maxTarDeviation = 0.5f;
+	// Easing for these deviations
+	public string           deviationEasing = Easing.Out;
+	public float passingAccuracy = .7f;
+	public bool checkToDeletePlayerPrefs = false;
+	
+	public bool              ________________;
+
+	public Rect camRectNormal; 
+	public int               shotNum;
+	public GUIText           shotCounter, shotRating;
+	public GUITexture        checkMark;
+	public Shot lastShot;
+	public int numShots;
+	public Shot[] playerShots;
+	public float[] playerRatings;
+	public GUITexture whiteOut;
+
+	void Awake(){
+		S = this;
+	}
+	void Start() {
+		// Find the GUI components
+		GameObject go = GameObject.Find("ShotCounter");
+		shotCounter = go.GetComponent<GUIText>();
+		go = GameObject.Find("ShotRating");
+		shotRating = go.GetComponent<GUIText>();
+		go = GameObject.Find("_Check_64");
+		checkMark = go.GetComponent<GUITexture>();
+		go = GameObject.Find ("WhiteOut");
+		whiteOut = go.GetComponent<GUITexture> ();
+		// Hide the checkMark and whiteout
+		checkMark.enabled = false;
+		whiteOut.enabled = false;
+		
+		// Load all the shots from PlayerPrefs
+		Shot.LoadShots();
+		// If there were shots stored in PlayerPrefs
+		if (Shot.shots.Count>0) {
+			shotNum = 0;
+			ResetPlayerShotsAndRatings();
+			ShowShot(Shot.shots[shotNum]);
+		}
+		
+		// Hide the cursor (Note: this doesn't work in the Unity Editor unless
+		//  the Game pane is set to Maximize on Play.)
+		Cursor.visible = false;
+		camRectNormal = GetComponent<Camera>().rect;
+	}
+
+	void ResetPlayerShotsAndRatings() {
+		numShots = Shot.shots.Count;
+		// Initialize playerShots & playerRatings with default values
+		playerShots = new Shot[numShots];
+		playerRatings = new float[numShots];
+	} 
+			
+	void Update () {
+		Shot sh;
+		// Mouse Input
+		if (Input.GetMouseButtonDown (0) || Input.GetMouseButtonDown (1)) {
+			sh = new Shot ();
+			// Grab the position and rotation of fpCamera
+			sh.position = fpCamera.transform.position;
+			sh.rotation = fpCamera.transform.rotation;
+			// Shoot a ray from the camera and see what it hits
+			Ray ray = new Ray (sh.position, fpCamera.transform.forward);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				sh.target = hit.point;
+			}
+			if (editMode) {
+				if (Input.GetMouseButtonDown (0)) {
+					// Left button records a new shot
+					Shot.shots.Add (sh);
+					shotNum = Shot.shots.Count - 1;
+				} else {
+					//(Input.GetMouseButtonDown(1))
+					// Right button replaces the current shot
+					Shot.ReplaceShot (shotNum, sh);
+					ShowShot (Shot.shots [shotNum]);
+				}
+				ResetPlayerShotsAndRatings ();
+			} else {
+				// Test this shot against the current Shot
+				float acc = Shot.Compare (Shot.shots [shotNum], sh);
+				lastShot = sh;
+				playerShots [shotNum] = sh;
+				playerRatings [shotNum] = acc;
+				ShowShot (sh);
+				Invoke ("ShowCurrentShot", 1);
+			} 
+			this.GetComponent<AudioSource> ().Play ();
+		
+			// Position _TargetCamera with the Shot
+			//ShowShot(sh);
+			//Utils.tr(sh.ToXML());
+			// Record a new shot
+			//	Shot.shots.Add(sh);
+			//shotNum = Shot.shots.Count-1;
+			
+		}
+		
+		// Keyboard Input
+		// Use Q and E to cycle Shots
+		// Note: Either of these will throw an error if Shot.shots is empty.
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			shotNum--;
+			if (shotNum < 0) shotNum = Shot.shots.Count-1;
+			ShowShot(Shot.shots[shotNum]);
+		}
+		if (Input.GetKeyDown(KeyCode.E)) {
+			shotNum++;
+			if (shotNum >= Shot.shots.Count) shotNum = 0;
+			ShowShot(Shot.shots[shotNum]);
+		}
+		// If in editMode & Left Shift is held down...
+		if (editMode && Input.GetKey(KeyCode.LeftShift)) {
+			// Use Shift-S to Save
+			if (Input.GetKeyDown(KeyCode.S)) {
+				Shot.SaveShots();
+			}
+			// Use Shift-X to output XML to Console
+			if (Input.GetKeyDown(KeyCode.X)) {
+				Utils.tr(Shot.XML);
+			}
+		}
+		// Hold Tab to maximize the Target window
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			// Maximize when Tab is pressed
+			GetComponent<Camera>().rect = new Rect(0,0,1,1);
+		}
+		if (Input.GetKeyUp(KeyCode.Tab)) {
+			// Return to normal when Tab is released
+			GetComponent<Camera>().rect = camRectNormal;
+		} 
+		
+		// Update the GUITexts
+		shotCounter.text = (shotNum+1).ToString()+" of "+Shot.shots.Count;
+		if (Shot.shots.Count == 0) shotCounter.text = "No shots exist";
+		// ^ Shot.shots.Count doesn't require .ToString() because it is assumed
+		//  when the left side of the + operator is a string
+		//shotRating.text = ""; // This line will be replaced later
+		if (playerRatings.Length > shotNum && playerShots[shotNum] != null) {
+			float rating = Mathf.Round(playerRatings[shotNum]*100f);
+			if (rating < 0) rating = 0;
+			shotRating.text = rating.ToString()+"%";
+			checkMark.enabled = (playerRatings[shotNum] > passingAccuracy);
+			// ^ the > comparison is used to generate true or false
+		} else {
+			shotRating.text = "";
+			checkMark.enabled = false;
+		}
+	}
+	
+	
+	public void ShowShot(Shot sh) {
+		StartCoroutine (WhiteOutTargetWindow ());
+		// Position _TargetCamera with the Shot
+		transform.position = sh.position;
+		transform.rotation = sh.rotation;
+	}
+
+	public void ShowCurrentShot() {
+		ShowShot(Shot.shots[shotNum]);
+	}
+	
+	// Another use for coroutines is to have a fire-and-forget function with a
+	//  delay in it as we've done here. WhiteOutTargetWindow() will enable
+	//  whiteOut, yield for 0.05 seconds, and then disable it. Compare this
+	//  method of delay to the Invoke("ShowCurrentShot",1f) used above
+	public IEnumerator WhiteOutTargetWindow() {
+		whiteOut.enabled = true;
+		yield return new WaitForSeconds(0.05f);
+		whiteOut.enabled = false;
+	}
+			
+			// OnDrawGizmos() is called ANY time Gizmos need to be drawn, even when
+		//  Unity isn't playing!
+	public void OnDrawGizmos() {
+		List<Shot> shots = Shot.shots;
+		for (int i=0; i<shots.Count; i++) {
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(shots[i].position, 0.5f);
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine( shots[i].position, shots[i].target );
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(shots[i].target, 0.25f);
+		}
+		// If checkToDeletePlayerPrefs is checked
+		if (checkToDeletePlayerPrefs) {
+			Shot.DeleteShots(); // Delete all the shots
+			// Uncheck checkToDeletePlayerPrefs
+			checkToDeletePlayerPrefs = false;
+			shotNum = 0; // Set shotNum to 0
+		}
+	// Show the player's last shot attempt
+	if (lastShot != null) {
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere(lastShot.position, 0.25f);
+		Gizmos.color = Color.white;
+		Gizmos.DrawLine( lastShot.position, lastShot.target );
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(lastShot.target, 0.125f); 
+		}
+	}
+}
